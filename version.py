957520de373d9e66f8428e24ec952ce08397376d
Javiera Inostroza <javinostroz@gmail.com>
2025-05-13 12:10:57 -0400

    Add files via upload

---
+++ version.py
@@ -0,0 +1,23 @@
+""" version MAJOR.MINOR.PATCH
+EJEMPLO V2.4.1
+
+MAJOR: (version mayor): Se incrementa cuando se hacen cambios grandes (Generalmente incompatibles) con la versión anterior.
+MINOR: (version menor): Se incrementa cuando se agregan nuevas funcionalidades al sistema pero sin romper la compatibilidad.
+PATCH: (parche o revision): Se incrementa cuando se corrigen errores en el sistema o se mejoran funcionalidades
+"""
+
+#Historial 
+#       15/04/2025 Inicio del desarrollo v1.0.0
+#       22/04/2025 Agrega la opcion 5 Modificar Cantidad v1.1.0
+#       22/04/2025 Mejora las funciones de 3 y 4 al buscar con while v1.1.1
+#       29/04/2025 Cambio de Paradigma, inicio el trabajo con funciones v2.0.0
+#       05/05/2025 Se reemplaza el buscar y mostrar producto por funciones y se agrega control de keyboardInterrupt v2.0.1
+#       13/05/2025 Cambio de código a módulos v2.1.1
+
+
+#   version = "v1.0.0"
+#   version = "v1.1.0"
+#   version = "v1.1.1"
+#   version = "v2.0.0"
+#   version = "v2.0.1"
+version = "v2.1.1"
